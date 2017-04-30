88d6600c618026b280cf9546e90aa53c0882a437
guforiptal <guforiptal@mail.ru>
2017-04-30 22:01:33 +0300

    improved grid functionality

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -13,6 +13,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use AppBundle\Entity\User;
+use Symfony\Component\Debug\Debug;
+
 
 class AdminController extends Controller
 {
@@ -36,13 +38,13 @@ class AdminController extends Controller
         if (!$sidx) $sidx = 1;
 
         $start = $limit * $page - $limit;
-        if($start < 0) $start = 0;
+        if ($start < 0) $start = 0;
 
         $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery(
             'SELECT u
     FROM AppBundle:User u
-    ORDER BY u.'.$this->caseCorrector($sidx).' '.strtoupper($sord)
+    ORDER BY u.' . $this->caseCorrector($sidx) . ' ' . strtoupper($sord)
         )->setFirstResult($start)->setMaxResults($limit);
 
         $count = $em->createQuery(
@@ -50,8 +52,8 @@ class AdminController extends Controller
     FROM AppBundle:User u'
         )->getSingleScalarResult();
 
-        if( $count > 0 && $limit > 0) {
-            $total_pages = ceil($count/$limit);
+        if ($count > 0 && $limit > 0) {
+            $total_pages = ceil($count / $limit);
         } else {
             $total_pages = 0;
         }
@@ -76,13 +78,13 @@ class AdminController extends Controller
             $s .= "<cell>" . $user->getEmailCanonical() . "</cell>";
             $s .= "<cell>" . $user->isEnabled() . "</cell>";
             $s .= "<cell>" . $user->getSalt() . "</cell>";
-            if($user->getLastLogin() != null)  $s .= "<cell>" . $user->getLastLogin()->format('Y-m-d H:i:s') . "</cell>";
+            if ($user->getLastLogin() != null) $s .= "<cell>" . $user->getLastLogin()->format('Y-m-d H:i:s') . "</cell>";
             else $s .= "<cell>" . 'N/A' . "</cell>";
             $s .= "<cell>" . $user->getConfirmationToken() . "</cell>";
-            if($user->getPasswordRequestedAt() != null)   $s .= "<cell>" . $user->getPasswordRequestedAt()->format('Y-m-d H:i:s') . "</cell>";
+            if ($user->getPasswordRequestedAt() != null) $s .= "<cell>" . $user->getPasswordRequestedAt()->format('Y-m-d H:i:s') . "</cell>";
             else $s .= "<cell>" . 'N/A' . "</cell>";
             $roles = "";
-            foreach ($user->getRoles() as $str){
+            foreach ($user->getRoles() as $str) {
                 $roles .= $str . ' ';
             }
             $s .= "<cell>" . $roles . "</cell>";
@@ -90,6 +92,46 @@ class AdminController extends Controller
         }
         $s .= "</rows>";
         return new Response($s);
+    }
+
+    /**
+     * @Route("/admin/users/edit", name="users_edit")
+     */
+    public function usersActionEdit()
+    {
+        $id = $_POST['id'];
+        $column_name = $this->getColumnName();
+        $value = $_POST[$column_name];
+
+        //file_put_contents("php://stdout", "\nDump:");
+        //$this->printArray(array_keys($_POST));
+
+        $repository = $this->getDoctrine()->getRepository('AppBundle:User');
+        $user = $repository->findOneById($id);
+
+        $setter_name = 'set'.ucfirst($this->caseCorrector($column_name));
+        $user->$setter_name($value);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return new Response();
+    }
+
+    private function getColumnName()
+    {
+        return array_keys($_POST)[0];
+    }
+
+    private function printArray($array)
+    {
+        foreach ($array as $key => $value) {
+            file_put_contents("php://stdout", "\n$key => $value" . var_dump($_POST));
+            if (is_array($value)) {
+                $this->printArray($value);
+            }
+        }
     }
 
     private function caseCorrector($str)
@@ -107,30 +149,5 @@ class AdminController extends Controller
                 return 'passwordRequestedAt';
         }
         return $str;
-    }
-
-    /**
-     * @Route("/admin/users/edit", name="users_edit")
-     */
-    public function usersActionEdit()
-    {
-        $id = $_POST['id'];
-        $username = $_POST['username'];
-
-        $repository = $this->getDoctrine()->getRepository('AppBundle:User');
-        $user = $repository->findOneById($id);
-        $user->setUsername($username);
-        $user->setUsernameCanonical($username);
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($user);
-        $em->flush();
-
-        $s = "";
-        foreach($_POST as $key=>$value){
-            $s .= $key.' '.$value."\n";
-        }
-
-        return new Response();
     }
 }
